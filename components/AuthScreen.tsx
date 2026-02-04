6b7d95cbd8b222ada0067c737b2fa3da01a987cf
max-gorbie <gorbachev.maxim@gmail.com>
2026-02-05 01:16:52 +0300

    icon-13px

--- components/AuthScreen.tsx
+++ components/AuthScreen.tsx
@@ -47,7 +47,7 @@ const AuthScreen: React.FC<AuthScreenProps> = ({ onLogin }) => {
                                 type={showPassword ? "text" : "password"}
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
-                                className="w-[120px] md:w-[140px] pl-0 pr-7 py-0 bg-[#1E1E1E] border-none rounded-md focus:outline-none focus:ring-0 focus:ring-white text-white text-left text-[12px] md:text-[12px]"
+                                className="w-[120px] md:w-[140px] pl-0 pr-7 py-0 bg-[#1E1E1E] border-none rounded-md focus:outline-none focus:ring-0 focus:ring-white text-white text-left text-[12px] md:text-[13px]"
                                 placeholder=" "
                                 autoFocus
                             />
@@ -57,12 +57,12 @@ const AuthScreen: React.FC<AuthScreenProps> = ({ onLogin }) => {
                                 className="absolute right-1.5 top-1/2 -translate-y-1/2 text-neutral-400 hover:text-white transition-colors focus:outline-none"
                             >
                                 {showPassword ? (
-                                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
                                         <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
                                         <line x1="1" y1="1" x2="23" y2="23" />
                                     </svg>
                                 ) : (
-                                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
                                         <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
                                         <circle cx="12" cy="12" r="3" />
                                     </svg>
